e3dcd633a853c36879fd79c33b6eb60fbc707f51
unknown <you@example.com>
2025-01-16 16:36:54 +0500

    project start

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav>
+        <div className="navLogo">ChillPro<span>Service</span></div>
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
