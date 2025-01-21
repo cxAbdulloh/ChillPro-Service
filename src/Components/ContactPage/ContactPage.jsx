47730109ffd32a37c7423bf556cf9f0d24e073bb
unknown <you@example.com>
2025-01-21 20:31:47 +0500

    fix the bugs

--- src/Components/ContactPage/ContactPage.jsx
+++ src/Components/ContactPage/ContactPage.jsx
@@ -1,11 +1,12 @@
 import { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import { useNavigate } from 'react-router-dom';  // useNavigate import qilinadi
 import './ContactPage.css'
 import Navbar from '../Navbar/Navbar';
-import { refresh } from 'aos';
 
-export const ContactPage= () => {
+export const ContactPage = () => {
   const form = useRef();
+  const navigate = useNavigate();  // useNavigate hookini chaqiramiz
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -17,6 +18,8 @@ export const ContactPage= () => {
       .then(
         () => {
           console.log('SUCCESS!');
+          // Email muvaffaqiyatli yuborilgandan so‘ng, Home sahifasiga yo‘naltiramiz
+          navigate('/');  // Bu yerda Home sahifasi (/) ga yo'naltirish amalga oshiriladi
         },
         (error) => {
           console.log('FAILED...', error.text);
@@ -24,26 +27,22 @@ export const ContactPage= () => {
       );
   };
 
- 
-
-
   return (
     <>
-    <Navbar/>
-    <div className='login-popup'>
-      <form className="login-popup-container" ref={form} onSubmit={sendEmail}>
-      <div className="login-popup-title">
-        <h1 className='lll'>ChillProService</h1>
-        </div>
-        <div className="login-popup-inputs" >
-            <input type="text" placeholder='Ismingiz*' name='to_name'/>
-            <input type="tel" placeholder='+998*' name='to_number'/>
-        </div>
-        <button>Yuborish</button>
-      </form>
-    </div>
+      <Navbar />
+      <div className='login-popup'>
+        <form className="login-popup-container" ref={form} onSubmit={sendEmail}>
+          <div className="login-popup-title">
+            <h1 className='lll'>ChillProService</h1>
+          </div>
+          <div className="login-popup-inputs">
+            <input type="text" placeholder='Ismingiz*' name='to_name' />
+            <input type="tel" placeholder='+998*' name='to_number' />
+          </div>
+          <button type="submit">Yuborish</button>
+        </form>
+      </div>
     </>
-    
   );
 };
 
